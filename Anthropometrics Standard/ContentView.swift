7c12ba58ebcf40e91f2b01b146aa7aaa8cdcef09
Tom Bowden <tom.bowden@gmail.com>
2019-08-31 15:00:11 +0900

    Initial Commit

---
+++ Anthropometrics Standard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Anthropometrics Standard
+//
+//  Created by BowdenTom on 2019/08/31.
+//  Copyright © 2019 Mirai LLP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
